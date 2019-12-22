68907962dfbcba10b6141062f3aa70ccf48e5867
Richard Poettler <richard.poettler@gmail.com>
2019-12-22 11:07:06 +0100

    Preliminary doom/emacs configuration

--- doom.d/config.el
+++ doom.d/config.el
@@ -1,3 +1,15 @@
 ;;; .doom.d/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here
+
+;; (global-display-line-numbers-mode)
+(setq display-line-numbers-type 'relative)
+
+;; (setq-default show-trailing-whitespace t)
+;; (setq-default indicate-empty-lines t)
+;; (add-hook 'before-save-hook 'delete-trailing-whitespace)
+;; (add-hook 'before-save-hook 'doom/delete-trailing-newlines)
+
+(map! :leader
+      :prefix "c"
+      :desc "Comment" "k" #'comment-dwim)
